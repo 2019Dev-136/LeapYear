e6a4d0e3ead2db1326cffe660ce51138bf5567a3
Harshit Sharma <harshitsharma@harshits-macbook-pro.local>
2019-03-14 22:04:39 +0530

    Initial Commit

---
+++ LeapYear/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LeapYear
+//
+//  Created by Harshit Sharma on 3/14/19.
+//  Copyright © 2019 2019_Dev_136. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ LeapYearTests/LeapYearTests.swift
@@ -0,0 +1,34 @@
+//
+//  LeapYearTests.swift
+//  LeapYearTests
+//
+//  Created by Harshit Sharma on 3/14/19.
+//  Copyright © 2019 2019_Dev_136. All rights reserved.
+//
+
+import XCTest
+@testable import LeapYear
+
+class LeapYearTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
