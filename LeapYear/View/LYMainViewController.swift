9fdb537f2932366ee7f25e261e8dc0d59bc3ed4f
2019Dev-136 <hvsharma07@gmail.com>
2019-03-17 16:09:40 +0530

    Updated LYMainViewController. Added check for user input validation and leap year in find_Clicked() and accordingly displaying alert

--- LeapYear/View/LYMainViewController.swift
+++ LeapYear/View/LYMainViewController.swift
@@ -11,13 +11,29 @@ import UIKit
 class LYMainViewController: UIViewController {
 
     @IBOutlet weak var yearTextField: UITextField!
+    private let presenter = LYPresenter()
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func find_Clicked(_ sender: Any) {
-        
+        presenter.enteredYear = yearTextField.text!       
+        if presenter.validateUserInput() {
+            let isLeapYear = presenter.isLeapYear()
+            isLeapYear ? displayAlert(title: LYConstantMessages.alertTitle, message: LYConstantMessages.leapYear) : displayAlert(title: LYConstantMessages.alertTitle, message: LYConstantMessages.notLeapYear)
+        }
+        else {
+            displayAlert(title: LYConstantMessages.alertTitle, message: LYConstantMessages.invalidEntry)
+            return
+        }
+    }
+    
+    func displayAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction (title: LYConstantMessages.oKText, style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
     
 }
