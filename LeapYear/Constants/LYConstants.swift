5bc096da0a701d2981a1b262329176ddf3a51b08
2019Dev-136 <hvsharma07@gmail.com>
2019-03-17 13:49:13 +0530

    Added constant file i.e. LYConstants.swift in project

---
+++ LeapYear/Constants/LYConstants.swift
@@ -0,0 +1,14 @@
+//
+//  LYConstants.swift
+//  LeapYear
+//
+//  Created by 2019_Dev_136 on 3/15/19.
+//  Copyright © 2019 2019_Dev_136. All rights reserved.
+//
+
+import Foundation
+
+struct LYConstants {
+    static let four = 4
+    static let allowedCharacter = CharacterSet(charactersIn: "1234567890")
+}
