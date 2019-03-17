403d4054b9acb8df5478589867d6e5c603ccf3fc
2019Dev-136 <hvsharma07@gmail.com>
2019-03-17 15:10:31 +0530

    Updated LYConstants file with hundred and fourHundred values

--- LeapYear/Constants/LYConstants.swift
+++ LeapYear/Constants/LYConstants.swift
@@ -10,5 +10,7 @@ import Foundation
 
 struct LYConstants {
     static let four = 4
+    static let hundred = 100
+    static let fourHundred = 400
     static let allowedCharacter = CharacterSet(charactersIn: "1234567890")
 }
